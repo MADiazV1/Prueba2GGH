f8da33dc3105028d7eac1b45f928c3a4c37246f5
Miguel Ángel Díaz Valencia <diazvalenciam9@gmail.com>
2022-07-03 17:04:37 -0500

    Dandole un estilo mas detallado a la pagina con un ciclo en index.js para compribar que el fonde de pantalla estaba fijo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ alert("Hello world");
 
 var miNombre = function(){
     var nombre = "MIGUEL"
-    for(var i=0;i<3;i++){
+    for(var i=0;i<40;i++){
         document.write("Hola, "+nombre+'<br>');
     }
 }
